faf0349db98bf0aba0715e46eeb9b234afe09e30
dudekrystian <dudekrystian@gmail.com>
2024-03-11 18:43:55 +0100

    build menu navigations

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -3,14 +3,53 @@
 import React from 'react'
 import styled from 'styled-components';
 import {useGlobalState} from "@/app/context/globalProvider.js"
+import Image from 'next/image';
+import Link from 'next/link';
+
+import menu from "@/app/utils/menu.js";
+import { usePathname, useRouter } from "next/navigation";
 
 function Sidebar() {
 
   const {theme} = useGlobalState()
 
+  const router = useRouter();
+  const pathname = usePathname();
+
+
+  const handleClick = (link: string) => {
+    router.push(link);
+  }
+
   return (
     <SidebarStyled theme={theme}>
-    Sidebar
+    <div className="profile">
+      <div className="profile-overlay"></div>
+      <div className="image">
+      <Image width={70} height={70} src="/avatar1.png" alt="profile" />
+      </div>
+      <h1>
+        <span>John</span>
+        <span>Novak</span>
+      </h1>
+    </div>
+    <ul className="nav-items">
+    {menu.map((item) => {
+          const link = item.link;
+          return (
+            <li
+              key={item.id}
+              className={`nav-item ${pathname === link ? "active" : ""}`}
+              onClick={() => {
+                handleClick(link);
+              }}
+            >
+              {item.icon}
+              <Link href={link}>{item.title}</Link>
+            </li>
+          );
+        })}
+    </ul>
     </SidebarStyled>
   )
 }
@@ -19,7 +58,7 @@ const SidebarStyled =  styled.nav`
     position: relative;
     width: ${(props)=> props.theme.sidebarWidth};
     background-color: ${(props) => props.theme.colorBg2};
-    border-right: 2px solid ${(props)=> props.theme.borderColor};
+    border 2px solid ${(props)=> props.theme.borderColor};
     border-radius: 1 rem;
 
 `;
