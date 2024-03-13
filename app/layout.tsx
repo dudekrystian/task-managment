a60a12220d7ad741967790ba0998a6fbb4788b69
dudekrystian <dudekrystian@gmail.com>
2024-03-13 21:26:24 +0100

    Add authentication by wrapping app

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ import Sidebar from "./Components/Sidebar/Sidebar";
 import GlobalStylesProvider from "./providers/GlobalStylesProvider";
 import ContextProvider from "./providers/ContextProvider";
 
+import { ClerkProvider } from '@clerk/nextjs'
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -37,5 +41,7 @@ export default function RootLayout({
 
       </body>
     </html>
+    </ClerkProvider>
+
   );
 }
