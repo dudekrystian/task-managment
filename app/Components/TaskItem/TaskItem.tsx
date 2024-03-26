d7b94ee5c2779f202ab01a8d4c9180dc8399fe7f
dudekrystian <dudekrystian@gmail.com>
2024-03-26 20:28:30 +0100

    import formatDate and wrapp

--- app/Components/TaskItem/TaskItem.tsx
+++ app/Components/TaskItem/TaskItem.tsx
@@ -3,6 +3,7 @@ import { useGlobalState } from "@/app/context/globalProvider";
 import { edit, trash } from "@/app/utils/Icons";
 import React from "react";
 import styled from "styled-components";
+import formatDate from "@/app/utils/formatDate";
 
 interface Props {
   title: string;
