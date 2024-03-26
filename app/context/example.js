545244b025152485825bbe2ace10c93fa347fad1
dudekrystian <dudekrystian@gmail.com>
2024-03-26 20:29:05 +0100

    example tasks

---
+++ app/context/example.js
@@ -0,0 +1,46 @@
+const exampleTasks = [
+  {
+    id: 1,
+    title: "Doe",
+    description: "blablalblabla",
+    date: "26/03/2024",
+    isCompleted: "false",
+  },
+  {
+    id: 2,
+    title: "Doe",
+    description: "blablalblabla",
+    date: "26/03/2024",
+    isCompleted: "false",
+  },
+  {
+    id: 3,
+    title: "Doe",
+    description: "blablalblabla",
+    date: "26/03/2024",
+    isCompleted: "true",
+  },
+  {
+    id: 4,
+    title: "Doe",
+    description: "blablalblabla",
+    date: "26/03/2024",
+    isCompleted: "false",
+  },
+  {
+    id: 3,
+    title: "Doe",
+    description: "blablalblabla",
+    date: "26/03/2024",
+    isCompleted: "true",
+  },
+  {
+    id: 4,
+    title: "Doe",
+    description: "blablalblabla",
+    date: "26/03/2024",
+    isCompleted: "false",
+  },
+];
+
+export default exampleTasks;
