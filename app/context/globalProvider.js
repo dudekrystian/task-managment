d36a863f15c60409bf90865d6e5799e297724f7c
dudekrystian <dudekrystian@gmail.com>
2024-04-06 20:01:19 +0200

    create global function update,delete Tasks and function for Modal

--- app/context/globalProvider.js
+++ app/context/globalProvider.js
@@ -14,9 +14,17 @@ export const GlobalProvider = ({ children }) => {
 
   const [selectedTheme, setSelectedTheme] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [modal, setModal] = useState(false);
   const [tasks, setTasks] = useState([]);
   const theme = themes[selectedTheme];
+
+  const openModal = () => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
 
   const allTasks = async () => {
     setIsLoading(true);
@@ -40,6 +48,23 @@ export const GlobalProvider = ({ children }) => {
     }
   };
 
+  const updateTask = async (task) => {
+    try {
+      const res = await axios.put(`/api/tasks`, task);
+
+      toast.success("Task updated");
+
+      allTasks();
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  const completedTasks = tasks.filter((task) => task.isCompleted === true);
+  const importantTasks = tasks.filter((task) => task.isImportant === true);
+  const incompleteTasks = tasks.filter((task) => task.isCompleted === false);
+
   useEffect(() => {
     if (user) {
       allTasks();
@@ -53,6 +78,13 @@ export const GlobalProvider = ({ children }) => {
         tasks,
         deleteTask,
         isLoading,
+        completedTasks,
+        importantTasks,
+        incompleteTasks,
+        updateTask,
+        modal,
+        openModal,
+        closeModal,
       }}
     >
       <GlobalUpdateContext.Provider value={{}}>
