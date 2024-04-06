94c5e73592e4c689bd4d1fc5a89bf0f59a8e34b1
dudekrystian <dudekrystian@gmail.com>
2024-04-06 20:00:09 +0200

    build page for incomplete Tasks

--- app/incomplete/page.tsx
+++ app/incomplete/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import React from 'react'
+import { useGlobalState } from '../context/globalProvider';
+import Tasks from '../Components/Tasks/Tasks';
 
 function page() {
+  const {incompleteTasks} = useGlobalState();
+
   return (
-    <div>incomplete</div>
+    <Tasks title='Incomplete Tasks' tasks={incompleteTasks} />
   )
 }
 
