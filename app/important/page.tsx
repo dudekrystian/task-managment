051eba18e763234c27305220b7a776bf17efbe9c
dudekrystian <dudekrystian@gmail.com>
2024-04-06 19:59:51 +0200

    build page form important Tasks

--- app/important/page.tsx
+++ app/important/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import React from 'react'
+import { useGlobalState } from '../context/globalProvider';
+import Tasks from '../Components/Tasks/Tasks';
 
 function page() {
+  const { importantTasks} = useGlobalState();
+
   return (
-    <div>important</div>
+    <Tasks title='Important Tasks' tasks={importantTasks} />
   )
 }
 
